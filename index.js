15278747b2fc467ba5cb2ad868a9deaf1b269db3
Edward Xiong <53875786+Edidas02@users.noreply.github.com>
2023-07-24 00:14:12 -0700

    update

--- index.js
+++ index.js
@@ -203,23 +203,15 @@ function createChart2(data) {
     .attr("font-size", "12px");
   
     // Prepare data for the pie chart
-    const furnishingStatusData = d3.rollups(
-      data,
-      v => v.length,
-      d => d.furnishingstatus
+    const pieData = Array.from(
+      d3.rollups(
+        data,
+        v => v.length,
+        d => d.furnishingstatus
+      ),
+      ([category, count]) => ({ category, count })
     );
     
-    // Convert the Map-like data structure into an array of objects
-    const furnishedCount = data.filter(d => d.furnishingstatus === "furnished").length;
-    const semiFurnishedCount = data.filter(d => d.furnishingstatus === "semi-furnished").length;
-    const unfurnishedCount = data.filter(d => d.furnishingstatus === "unfurnished").length;
-  
-    // Prepare the data for the pie chart
-    const pieData = [
-      { category: "Furnished", count: furnishedCount },
-      { category: "Semi-Furnished", count: semiFurnishedCount },
-      { category: "Unfurnished", count: unfurnishedCount },
-    ];    
     console.log(pieData);
   
     // Set up dimensions for the pie chart
